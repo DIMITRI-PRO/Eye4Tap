6046691303d7a77cf1b3b01962f0ecfec82f20c2
CHOUMMANIVONGDimitri <dimitrichoummanivong@gmail.com>
2023-08-05 00:41:42 +0200

    🏗️ update : place execute controllers

--- backend/src/router.js
+++ backend/src/router.js
@@ -1,23 +1,25 @@
 import { Router, json } from "express";
-import users from "./models/Users/usersSchema.js";
 import auth from "./middleware/auth.js";
 import Controllers from "./controllers/index.js";
+import exectutor from "./utils/exectuteControllers.js";
+import models from "./models/index.js";
 
 const { Users } = Controllers;
+const { validateSchema } = models.Users;
+const { executeControllers } = exectutor;
 
 const router = Router();
 
 const { hashPassword, verifyPassword, verifyToken } = auth;
-const { validateUser } = users;
-const { userControllers, login, postUser } = Users;
+const { login, postUser } = Users;
 
 router.use(json());
 
-router.post("/register", validateUser, hashPassword, postUser);
+router.post("/register", validateSchema, hashPassword, postUser);
 router.post("/login", login, verifyPassword);
 
 router.use(verifyToken);
 
-userControllers(router);
+executeControllers(Controllers, router); // get all controllers in each folders with index.js
 
 export default router;
