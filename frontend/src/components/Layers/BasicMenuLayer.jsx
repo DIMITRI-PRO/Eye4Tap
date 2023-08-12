8aeb9e40d96d2011386c19d67999eaea07ab6824
CHOUMMANIVONGDimitri <dimitrichoummanivong@gmail.com>
2023-08-12 18:57:55 +0200

    ✅ update : props value

--- frontend/src/components/Layers/BasicMenuLayer.jsx
+++ frontend/src/components/Layers/BasicMenuLayer.jsx
@@ -1,10 +1,10 @@
 import PropTypes from "prop-types";
 
-export const BasicMenuLayer = ({ children, footer }) => {
+export const BasicMenuLayer = ({ children, footer, displayFooter }) => {
   return (
     <div className="ninja basic-layer">
       <main className="ninja basic-layer-children">{children}</main>
-      {footer?.()}
+      {displayFooter && footer?.()}
     </div>
   );
 };
@@ -12,7 +12,9 @@ export const BasicMenuLayer = ({ children, footer }) => {
 BasicMenuLayer.propTypes = {
   children: PropTypes.node.isRequired,
   footer: PropTypes.func,
+  displayFooter: PropTypes.bool,
 };
 BasicMenuLayer.defaultProps = {
   footer: null,
+  displayFooter: true,
 };
