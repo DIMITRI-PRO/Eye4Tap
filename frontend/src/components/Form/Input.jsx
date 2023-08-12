af6416260fef8edb047edb52dd066cd1a46976af
CHOUMMANIVONGDimitri <dimitrichoummanivong@gmail.com>
2023-08-12 18:56:50 +0200

    ✅ create : input component

---
+++ frontend/src/components/Form/Input.jsx
@@ -0,0 +1,42 @@
+import { useState, useEffect } from "react";
+import PropTypes from "prop-types";
+import { Eye, EyeOff } from "../../assets/FeatherIcons";
+
+export const Input = ({ ...props }) => {
+  const [displayText, setDisplayText] = useState("text");
+  const [show, setShow] = useState(false);
+
+  useEffect(() => {
+    if (show) setDisplayText("text");
+    else setDisplayText(null);
+  }, [show]);
+
+  return (
+    <div className="ninja form-item child-container">
+      <input
+        className={`ninja form-item child-${props?.name || ""}`}
+        {...props}
+        type={displayText || props?.type}
+      />
+      {props?.type === "password" && (
+        <div
+          className="ninja eye-password"
+          aria-hidden="true"
+          onClick={() => {
+            setShow(!show);
+          }}
+        >
+          {show ? <Eye /> : <EyeOff />}
+        </div>
+      )}
+    </div>
+  );
+};
+
+Input.propTypes = {
+  name: PropTypes.string,
+  type: PropTypes.string.isRequired,
+};
+Input.defaultProps = {
+  name: "",
+};
