c3512523cda0eb0483422655f591ba584cf5f1dc
CHOUMMANIVONGDimitri <dimitrichoummanivong@gmail.com>
2023-08-12 18:52:44 +0200

    ✅ import : custom component

--- frontend/src/components/Customs/index.jsx
+++ frontend/src/components/Customs/index.jsx
@@ -1,5 +1,9 @@
 import { GlitchTitle } from "./Glitch/GlitchTitle";
+import { UserHeader } from "./UserHeader/UserHeader";
+import { CountDown } from "./CountDown/CounDown";
 
 export default {
   GlitchTitle,
+  UserHeader,
+  CountDown,
 };
