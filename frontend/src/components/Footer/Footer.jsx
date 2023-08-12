ef367c20445e7f57452db7e443e8015af2757736
CHOUMMANIVONGDimitri <dimitrichoummanivong@gmail.com>
2023-08-12 18:55:46 +0200

    ✅ update : footer component

--- frontend/src/components/Footer/Footer.jsx
+++ frontend/src/components/Footer/Footer.jsx
@@ -1,22 +1,24 @@
+import { useTranslation } from "react-i18next";
+import { Github, LinkedIn } from "../../assets/FeatherIcons";
+
 export const FooterBar = () => {
+  const { t } = useTranslation();
   return (
-    <footer className="">
-      <div className="">
-        <ul className="">
+    <footer className="ninja footer-container">
+      <div className="ninja footer-main-content">
+        <ul id="footer-list-url">
           <li>
             <a
               href="https://github.com/CHOUMMANIVONGDimitri/Eye4Tap-version1.0"
-              className="6"
               target="_blank"
               rel="noreferrer"
             >
-              Github EYE4TAP
+              <Github /> Github EYE4TAP
             </a>
           </li>
           <li>
             <a
               href="https://webgazer.cs.brown.edu/"
-              className=""
               target="_blank"
               rel="noreferrer"
             >
@@ -25,36 +27,31 @@ export const FooterBar = () => {
           </li>
           <li>
             <a
-              href="https://rapidapi.com/Kurizu/api/any-anime"
+              href="https://www.linkedin.com/in/dimitri-choummanivong-507669228/"
               className=""
-              target="_blank"
-              rel="noreferrer"
             >
-              Any Anime API
+              <LinkedIn /> LinkedIn
             </a>
           </li>
           <li>
             <a
-              href="https://github.com/Bouman/react-repo-base"
-              className=""
+              href="https://github.com/DIMITRI-PRO/Ninja-Template"
               target="_blank"
               rel="noreferrer"
             >
-              Template Repo, Thanks to Yannick B.
+              <Github /> Template Repo.
             </a>
           </li>
         </ul>
       </div>
-      <hr className="" />
-      <span className="">
-        Eye-4-Tap™ version alpha 1.0{" "}
-        <a
-          href="https://www.linkedin.com/in/dimitri-choummanivong-507669228/"
-          className=""
-        >
-          CHOUMMANIVONG X. Dimitri, KUROTSUME ©
-        </a>
-        . All Rights Reserved.
+      <hr id="footer-separator" className="ninja separator" />
+      <span className="ninja footer right-reserved">
+        {t("footer.text-rights", {
+          app: "Eye-4-Tap",
+          version: "alpha 1.0",
+          name: "CHOUMMANIVONG X. Dimitri",
+          corp: "KUROTSUME",
+        })}
       </span>
     </footer>
   );
