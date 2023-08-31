5616e6fc526e7840b29526a378ffaabaace0089f
CHOUMMANIVONGDimitri <dimitrichoummanivong@gmail.com>
2023-08-31 09:20:01 +0200

    🎨 remove style attribut and add classname

--- frontend/src/components/Layers/NotificationMessage.jsx
+++ frontend/src/components/Layers/NotificationMessage.jsx
@@ -1,37 +1,50 @@
 import PropTypes from "prop-types";
 import { Button } from "../Buttons/Button";
 import { ProgressBar } from "../Progress/ProgressBar";
-import { X } from "../../assets/FeatherIcons";
+import { X, AlertCircle, CheckCircle } from "../../assets/FeatherIcons";
 
 export const NotificationMessage = ({
+  duration,
+  type,
   display,
   setDisplay,
   content,
   setErrors,
 }) => {
-  const divStyle = {
-    position: "fixed",
-    minHeight: 150,
-    minWidth: 300,
-    backgroundColor: "#fff",
-    bottom: 10,
-    left: 10,
-    borderRadius: "0.5rem",
-    boxShadow: "10px 10px 15px -5px rgba(0,0,0,0.3)",
-  };
-
   return (
     content &&
     display && (
-      <div style={divStyle} className="ninja notif-message">
+      <div
+        id="notif-message"
+        className="ninja notif-message"
+        style={{ animation: `fadeRight ${duration}s ease-out` }}
+      >
         <div className="ninja notif-close">
-          <Button name="close-notif" onClick={() => setDisplay(false)}>
+          <Button name="circle" onClick={() => setDisplay(false)}>
             <X />
           </Button>
         </div>
-        {content && <div className="ninja notif-content">{content}</div>}
+        {content && (
+          <div className="ninja notif-content">
+            {type === "fail" && (
+              <AlertCircle
+                width="1.1rem"
+                height="1.1rem"
+                className={`ninja notif-logo ${type}`}
+              />
+            )}
+            {type === "succes" && (
+              <CheckCircle
+                width="1.1rem"
+                height="1.1rem"
+                className={`ninja notif-logo ${type}`}
+              />
+            )}
+            <span>{content}</span>
+          </div>
+        )}
         <ProgressBar
-          duration={5}
+          duration={duration}
           action={() => {
             setDisplay(!display);
             setErrors([]);
@@ -47,6 +60,8 @@ NotificationMessage.propTypes = {
   setDisplay: PropTypes.func,
   setErrors: PropTypes.func,
   content: PropTypes.string,
+  type: PropTypes.string,
+  duration: PropTypes.number,
 };
 
 NotificationMessage.defaultProps = {
@@ -54,4 +69,6 @@ NotificationMessage.defaultProps = {
   setDisplay: () => {},
   setErrors: () => {},
   content: "",
+  type: "default",
+  duration: 5,
 };
