9c947e520749b700323c584533881997e49469d2
CHOUMMANIVONGDimitri <dimitrichoummanivong@gmail.com>
2023-08-31 10:03:46 +0200

    ✅ update import page

--- frontend/src/pages/index.jsx
+++ frontend/src/pages/index.jsx
@@ -1,9 +1,9 @@
 import { Home } from "./Public/Home/Home";
-import { Login } from "./Public/Login";
-import { NotFound } from "./Public/NotFound";
-import { Register } from "./Public/Register";
+import { Login } from "./Public/Login/Login";
+import { NotFound } from "./Public/NotFound/NotFound";
+import { Register } from "./Public/Register/Register";
 
 import { GamePage } from "./Private/Game/GamePage";
-import { Profile } from "./Private/Profile";
+import { Profile } from "./Private/Profile/Profile";
 
 export { Home, Login, NotFound, Register, Profile, GamePage };
