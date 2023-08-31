53ca5a2cfdac548d7c9a456a14993b2547a1a963
CHOUMMANIVONGDimitri <dimitrichoummanivong@gmail.com>
2023-08-31 10:43:32 +0200

    ✅ update controllers and use FileSystem func

--- backend/src/controllers/index.js
+++ backend/src/controllers/index.js
@@ -1,33 +1,7 @@
-import fs from "fs";
-import path, { dirname } from "path";
-import url, { fileURLToPath } from "url";
-import log from "../services/logger.js";
+import FileSystem from "../utils/FileSystem.js";
 
-const { loggingError } = log;
+const { getIndexDirectory, loadModules } = FileSystem;
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+const directory = getIndexDirectory(import.meta.url);
 
-const subDirs = fs
-  .readdirSync(__dirname, { withFileTypes: true, encoding: "utf-8" })
-  .filter((dirent) => dirent.isDirectory())
-  .map((dirent) => dirent.name);
-
-const modules = {};
-
-for (const subDir of subDirs) {
-  try {
-    const indexPath = path.join(__dirname, subDir, "index.js");
-    const indexPathUrl = url.pathToFileURL(indexPath);
-    // eslint-disable-next-line no-await-in-loop
-    const module = await import(indexPathUrl.href);
-    modules[subDir] = module.default;
-  } catch (err) {
-    loggingError(
-      "[Export: Failed] or [MissingFile: index.js]",
-      ` ${__dirname}\\${subDir}`
-    );
-  }
-}
-
-export default modules;
+export default await loadModules(directory);
