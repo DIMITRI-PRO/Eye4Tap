96e0cc04b6044c53931c575c48801ac00c1d102b
CHOUMMANIVONGDimitri <dimitrichoummanivong@gmail.com>
2023-08-31 10:39:42 +0200

    ✅ update execute controllers

--- backend/src/utils/exectuteControllers.js
+++ backend/src/utils/exectuteControllers.js
@@ -1,9 +1,39 @@
-const executeControllers = (obj, router) => {
-  Object.keys(obj).forEach((key) => {
-    if (key.includes("Controllers") && typeof obj[key] === "function")
-      obj[key](router);
+import { Router } from "express";
+import log from "../services/logger.js";
 
-    if (typeof obj[key] === "object") executeControllers(obj[key], router);
+const { loggingError } = log;
+
+const executeControllers = (obj, router, index = 0) => {
+  const result = obj;
+
+  Object.entries(result).forEach(([key, value]) => {
+    const keyValue = value;
+
+    if (
+      result?.routeName &&
+      key.includes("Controllers") &&
+      typeof value === "function"
+    ) {
+      const allCurrentRoutes = result[key](Router());
+      router.use(`/${result.routeName}`, allCurrentRoutes);
+    }
+
+    if (typeof value === "object") {
+      keyValue.routeName = key;
+      keyValue.index = index;
+
+      if (
+        index === 0 &&
+        keyValue.routeName === keyValue.routeName.toLowerCase()
+      ) {
+        executeControllers(result[key], router, index + 1);
+      } else {
+        loggingError(
+          "Controllers generation status - [Failed] : ",
+          `the folder named ${key} have to be on lower case...`
+        );
+      }
+    }
   });
 };
 
